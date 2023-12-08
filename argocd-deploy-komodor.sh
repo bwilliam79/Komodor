e36f8f43becf779d047fa5094b9bf29ca61d060f
Brandon Williams <brandon.williams79@gmail.com>
2023-12-08 11:13:16 -0600

    - Setting default sync options to automated and replace for Argo CD

--- argocd-deploy-komodor.sh
+++ argocd-deploy-komodor.sh
@@ -8,14 +8,6 @@ ARGOCD_PASSWORD="Komodor!"
 # Log file for output
 LOG_FILE=~/argocd-deploy-komodor.log
 
-# Install kubectl and helm
-#echo -e "Installing kubectl and helm.\n"
-#sudo snap install kubectl --classic 2>&1 > $LOG_FILE
-#sudo snap install helm --classic 2>&1 >> $LOG_FILE
-
-#shopt -s expand_aliases
-#alias kubectl="minikube kubectl --"
-
 echo -e "Deploying Argo CD.\n"
 kubectl create namespace argocd > $LOG_FILE 2>&1
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml >> $LOG_FILE 2>&1
@@ -40,7 +32,6 @@ echo -e "Registering minikube cluster with Argo CD.\n"
 argocd cluster add minikube >> $LOG_FILE 2>&1
 
 echo -e "Deploying Komodor.\n"
-#HELM_API_KEY=11ab1ef7-673b-4064-ad71-e9e0a944372d USER_EMAIL=brandon.williams79@gmail.com bash <(curl -s -Ls https://raw.githubusercontent.com/komodorio/Install/master/install.sh)
 helm repo add komodorio https://helm-charts.komodor.io >> $LOG_FILE 2>&1
 helm repo update >> $LOG_FILE 2>&1
 helm upgrade --install k8s-watcher komodorio/k8s-watcher \
@@ -60,18 +51,12 @@ helm upgrade --install k8s-watcher komodorio/k8s-watcher \
     sleep 1
 done
 
-#echo -e "\n\nDeploying example app on Argo CD.\n"
-#kubectl config set-context --current --namespace=argocd >> $LOG_FILE 2>&1
-#kubectl create namespace guestbook >> $LOG_FILE 2>&1
-#argocd app create guestbook --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-server https://kubernetes.default.svc --dest-namespace guestbook >> $LOG_FILE 2>&1
-#argocd app sync guestbook >> $LOG_FILE 2>&1
-
 echo -e "\n\nDeploying nginx via Argo CD.\n"
 kubectl config set-context --current --namespace=argocd >> $LOG_FILE 2>&1
 kubectl create namespace web-services >> $LOG_FILE 2>&1
 argocd app create nginx --repo https://github.com/bwilliam79/Komodor-App.git --dest-server https://kubernetes.default.svc --path nginx --dest-namespace web-services >> $LOG_FILE 2>&1
 argocd app sync nginx >> $LOG_FILE 2>&1
-argocd app set nginx --sync-policy automated >> $LOG_FILE 2>&1
+argocd app set nginx --sync-policy automated --sync-option Replace=true >> $LOG_FILE 2>&1
 
 printf "You can now access the Argo CD dashboard at \033[33;32mhttp://$HOSTNAME:8080\033[33;37m\n"
 echo -e "\nUsername: admin"
