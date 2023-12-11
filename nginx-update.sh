61b02fe28c37a501be9a6acbfb9105b1743d2cdf
Brandon Williams <brandon.williams79@gmail.com>
2023-12-11 11:51:46 -0600

    - Changed replicas from 5 to 1

---
+++ nginx-update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# 12/07/2023 - Brandon Williams
+# Shell script to speed up Komodor demo environment
+
+# Password to use for Argo CD admin account
+ARGOCD_PASSWORD="Komodor!"
+
+# Log file for output
+LOG_FILE=./nginx-update.log
+
+echo -e "Logging in to Argo CD\n"
+argocd login komodor:8080 --insecure --username admin --password $ARGOCD_PASSWORD > $LOG_FILE 2>&1
+
+echo -e "Syncing nginx deployment via Argo CD.\n"
+argocd app sync nginx >> $LOG_FILE 2>&1
+
+echo -en "Waiting for nginx deployment to stabilize."
+until kubectl get pods -n web-services | grep nginx | grep -i 'running' | grep 1/1 >> $LOG_FILE 2>&1
+do
+    echo -n "."
+    sleep 2
+done
+
+echo -e "\n\nSetting up port forward for nginx.\n"
+kubectl port-forward --address 0.0.0.0 svc/nginx -n web-services 8088:80 > /dev/null 2>&1 &
